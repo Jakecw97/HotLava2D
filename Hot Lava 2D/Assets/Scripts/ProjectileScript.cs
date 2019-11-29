9eaac5066755a3e2dd37aca438beac38b708354a
Jakecw97 <g00349377@gmit.ie>
2019-11-29 11:17:38 +0000

    Prefabs
    Added prefabs for mutliple
    Added script for sulphur vents and drops. Need to add damage/possibly implement health. Need to work on the rope mechanic now. Will Implement lava wave and key/boulder mechanic later.

---
+++ Hot Lava 2D/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ProjectileScript : MonoBehaviour
+{
+    private float speed = 5;
+    private Rigidbody2D myRigidbody;
+    private Vector2 direction;
+    private Animator projectileAnimation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        projectileAnimation = GetComponent<Animator>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = direction * speed;
+    }
+
+    public void Initialize(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    //When the object leaves the camera the projectile gets deleted
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        projectileAnimation.SetTrigger("Destroy");
+        DestroyProjectile();
+        
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+       
+    }
+}
---
+++ Hot Lava 2D/Assets/Scripts/ProjectileShootScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShootScript : MonoBehaviour
+{
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+    //Disables  when off screen
+    private void OnBecameInvisible()
+    {
+        GetComponent<ProjectileShootScript>().enabled = false;
+    }
+    //Re enables  once they are visible
+    private void OnBecameVisible()
+    {
+        GetComponent<ProjectileShootScript>().enabled = true;
+    }
+}
