7846461257292fa4a22ea8b17fda1bb468f35436
Jakecw97 <g00349377@gmit.ie>
2019-11-29 04:53:10 +0000

    Added basic scripts
    Added a Level Manager Script and PlayerControl Script

---
+++ Hot Lava 2D/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float respawnDelay = 1;
+    public PlayerControl gamePlayer; //referring to player_move script and object it is attached to
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePlayer = FindObjectOfType<PlayerControl>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Respawn()
+    {
+        StartCoroutine("RespawnCoroutine");
+    }
+
+    public IEnumerator RespawnCoroutine()
+    {
+        gamePlayer.gameObject.SetActive(false); //disabling player object temp
+        yield return new WaitForSeconds(respawnDelay);
+        gameObject.gameObject.SetActive(true); //re-enabling player object
+        gamePlayer.transform.position = gamePlayer.respawnPoint; //setting position point to respawn point in player_move script
+
+    }
+
+}
---
+++ Hot Lava 2D/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    //variables
+    public float speed = 0f;
+    public float jumpSpeed = 6f;
+    private float movement = 0f;
+   // private float direction = 1f; //instantiated to allow projectiles to work
+    private Rigidbody2D rigidBody;
+    private bool isDead;
+    private bool facingRight;
+
+    public Transform groundCheckPoint; //Bottom of player, checking if theyre on the ground
+    public float groundCheckRadius; // Radius of Player ground check
+    public LayerMask groundLayer; // jump off things added to this layer
+    private bool isTouchingGround;
+
+    private Animator playerAnimation;
+    public Vector3 respawnPoint; //Store position of where player is going to respawn to
+    public LevelManager gameLevelManager;
+
+
+
+  //  private float waitTime = 3f;
+
+  
+
+
+    private void Start()
+    {
+        //Player assets
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+
+        //When game loads, respawn point is set to position of player
+      //  respawnPoint = transform.position;
+
+        gameLevelManager = FindObjectOfType<LevelManager>();
+
+    }
+
+    private void Update()
+    {
+        // if player is touching ground = true, if not = false
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        movement = Input.GetAxis("Horizontal");
+
+        if (!this.playerAnimation.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            //will only move if keys left or right are pressed
+            if (movement > 0f)
+            {  //right
+               // direction = 1f;
+                rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+
+
+            }
+            else if (movement < 0f)
+            { //left
+               // direction = -1f;
+                rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+
+
+            }
+            else
+            { //dont move if none of keys are pressed
+                rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            }
+            if (movement < 0f && !facingRight)
+            {
+                facingRight = true;
+                Flip();
+            }
+            else if (movement > 0f && facingRight)
+            {
+                facingRight = false;
+                Flip();
+            }
+
+
+            //Jump, can check all inputs in Edit->Project Settings->Input
+            if (Input.GetButtonDown("Jump") && isTouchingGround == true)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            }
+
+        }
+    }
+    private void Flip()
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+}
